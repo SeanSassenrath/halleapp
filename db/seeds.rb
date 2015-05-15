79d511943185c18c3cfd1139c44275a06b685f6b
SeanSassenrath <sean.sassenrath@gmail.com>
2015-05-15 14:02:43 -0700

    small style fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
 Spot.create(latitude: "37.771201", longitude: -122.450724)
 Spot.create(latitude: "37.759703", longitude: -122.428093)
+Spot.create(latitude: "37.774929", longitude: -122.419416)
+Spot.create(latitude: "37.806053", longitude: -122.410331)
+Spot.create(latitude: "37.765979", longitude: -122.406918)
+Spot.create(latitude: "37.781553", longitude: -122.415643)
+Spot.create(latitude: "37.774929", longitude: -122.419416)
+
